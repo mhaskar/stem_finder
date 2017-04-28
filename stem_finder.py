10917a35f885879aed9784b7a279c6ccd519e079
Mohammad Askar <m.askar@isecur1ty.org>
2017-04-28 18:49:32 +0300

    Update stem_finder.py

--- stem_finder.py
+++ stem_finder.py
@@ -2,9 +2,10 @@
 
 
 import snowballstemmer
+from termcolor import cprint
 
 words_with_stem = []
-
+words_number = 0
 def stem_finder(word):
 	st = snowballstemmer.stemmer('english');
 	if st.stemWord(word) != word:
@@ -15,10 +16,13 @@ data_file = open("example.txt","r")
 
 data = data_file.readlines()
 
+
 for line in data:
 		line_list_of_words = line.strip("\n").split(" ")
-		for word in line_list_of_words:
-			stem_finder(word)
-		
-print "[+]Words with stem found : %s"%len(words_with_stem)		
-print "[+]List of words : %s"%words_with_stem
+                for word in line_list_of_words:
+					words_number = words_number + 1
+					stem_finder(word)
+
+cprint("[+]Total words in file : %s"%words_number,"green")	
+cprint("[+]Number of words with stem : %s"%len(words_with_stem),"blue")
+#print "[+]List of words : %s"%words_with_stem
